0f1d4b2a37972b0456229629892853476632aeff
zero-penguin <arato1060@outlook.jp>
2023-03-26 13:28:43 +0900

    main

---
+++ javascript_basicフォルダ/data.js
@@ -0,0 +1,11 @@
+// 四則演算
+console.log(45 + 18);
+console.log(30 - 12);
+console.log(15 * 6);
+console.log(30 / 3);
+console.log(45 % 8);
+
+ // 文字列の連結
+ console.log('45' + '18');
+ console.log('今日の天気は' + '晴れ');
+ console.log('今日の天気は' + '曇り');
---
+++ javascript_basicフォルダ/variable.js
@@ -0,0 +1,12 @@
+// 変数の宣言
+let userName;
+
+// 値の代入
+userName = '侍太郎';
+
+// 変数の宣言・値の代入
+let userNumber = 55;
+
+ // コンソールへの出力
+ console.log(userName);
+ console.log(userNumber);
